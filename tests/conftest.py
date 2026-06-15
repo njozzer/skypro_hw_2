ba77d1eb872865e0539014dad0c7a8f92bace963
Max <maxi4o@mail.ru>
2026-06-15 22:28:57 +0300

    added tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,5 +8,9 @@ def category():
     return Category("","",[])
 
 @pytest.fixture
-def product():
-    return Product("Samsung","phone",1000)
+def product_1():
+    return Product("Samsung","phone",1000, 5)
+
+@pytest.fixture
+def product_2():
+    return Product("IPhone", "phone", 10000, 5)
--- tests/test_Product.py
+++ tests/test_Product.py
@@ -1,3 +1,9 @@
 import pytest
 
 from src.Product import Product
+
+
+def test_product(product_1):
+    assert product_1().name == "Samsung"
+    assert product_1().price == 1000
+
