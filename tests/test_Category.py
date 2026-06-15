58dbde6426673fbce81bb3e9fe116d950d33a8ca
Max <maxi4o@mail.ru>
2026-06-15 22:39:21 +0300

    finishing

--- tests/test_Category.py
+++ tests/test_Category.py
@@ -3,5 +3,12 @@ from src.Category import Category
 
 def test_category(category: Category) -> None:
     assert category.name == "Phones"
-    assert category.product_count == 1
-    assert category.category_count == 1
+    assert Category.product_count == 1
+    assert Category.category_count == 1
+
+
+def test_categories(category: Category, category_2: Category) -> None:
+    assert category.name == "Phones"
+    assert category_2.name == "Laptops"
+    assert Category.product_count == 4
+    assert Category.category_count == 2
